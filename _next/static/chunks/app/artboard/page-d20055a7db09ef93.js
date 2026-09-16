9c66bc21d3e6ecddb0e75886a5758c82c6f81d20
riaarainaa <83808110+riaarainaa@users.noreply.github.com>
2026-09-16 17:36:51 +0530

    Add files via upload

---
+++ _next/static/chunks/app/artboard/page-d20055a7db09ef93.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[648],{9186:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,9159,23)),Promise.resolve().then(n.bind(n,824))}},e=>{e.O(0,[980,248,441,255,358],()=>e(e.s=9186)),_N_E=e.O()}]);
